a55ecffb92ed38da23e43667c6f803775f458b16
1620928 <1620928@034lab04026.up.edu>
2016-12-05 21:51:07 -0200

    Atu ViewMenu

--- SistemaBancario/src/views/ViewMenu.java
+++ SistemaBancario/src/views/ViewMenu.java
@@ -12,35 +12,41 @@ import java.util.Scanner;
  * @author 1620928
  */
 public class ViewMenu {
-    
-   public class MenuInicioView {
-    Scanner scan = new Scanner(System.in);
-    
-   public int Menu(){
-        
-       
-        System.out.println("---------- Bem vindo ao Fernaron Bank ----------");
-        System.out.println("Digite 1 para Cliente e 2 para Administrador:");
-        
-        int op = scan.nextInt();
-        scan.nextLine();
-        
-        //if()
-        
-        
-        
-        System.out.println("1 - Saldo;");
-        System.out.println("2 - Extrato;");
-        System.out.println("3 - Depósito;");
-        System.out.println("4 - Saque;");
-        System.out.println("5 - Pesquisar Cliente por codigo");
-        System.out.println("6 - Voltar");
-        System.out.print("Informe a opção desejada: ");
-        
-        int op = scan.nextInt();
-        scan.nextLine();
-        
-        return op;
-        
+
+    public class MenuInicioView {
+
+        Scanner scan = new Scanner(System.in);
+
+        public int Menu() {
+
+            System.out.println("---------- Bem vindo ao Fernaron Bank ----------");
+            System.out.println("Digite 1 para Cliente e 2 para Administrador:");
+
+            int op = scan.nextInt();
+            scan.nextLine();
+
+            if (op == 1) {
+                System.out.println("1 - Saldo;");
+                System.out.println("2 - Extrato;");
+                System.out.println("3 - Depósito;");
+                System.out.println("4 - Saque;");
+                System.out.println("5 - Sair;");
+            }else{
+                if(op == 2){
+                    System.out.println("1 - Cadastrar Cliente;");
+                    System.out.println("2 - Alterar Cliente;");
+                    System.out.println("3 - Excluir Cliente;");
+                    System.out.println("4 - Exibir Clientes;");
+                }
+            }
+
+            
+            System.out.print("Informe a opção desejada: ");
+
+            int op = scan.nextInt();
+            scan.nextLine();
+
+            return op;
+
+        }
     }
-}
